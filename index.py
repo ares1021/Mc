5d305254a9a58182fe0b3088782afed795c57fd9
ares1021 <s1103018@gm.pu.edu.tw>
2022-12-24 11:07:50 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,33 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("KEY.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+from flask import Flask, render_template, request, make_response, jsonify
+from datetime import datetime, timezone, timedelta
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
+    return homepage
+
+@app.route("/read")
+def read():
+    Result = ""     
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.order_by("mail", direction=firestore.Query.DESCENDING).get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))

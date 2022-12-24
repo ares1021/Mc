38f8d5f0196ed57897789d04b8036aaddb7f74b9
ares1021 <s1103018@gm.pu.edu.tw>
2022-12-24 11:58:14 +0800

    make it better

--- index.py
+++ index.py
@@ -9,7 +9,6 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 from flask import Flask, render_template, request, make_response, jsonify
-from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
 @app.route("/")
